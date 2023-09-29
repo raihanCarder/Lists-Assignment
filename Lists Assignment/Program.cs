c938131dd747901d3ec02783c7299703485386c7
Raihan Carder <339509226@GoTVDSB.ca>
2023-09-29 11:02:10 -0400

    done first

--- Lists Assignment/Program.cs
+++ Lists Assignment/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
 using System.Linq;
+using System.Runtime.Remoting.Metadata;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,16 +12,19 @@ namespace Lists_Assignment
     {
         static void Main(string[] args)
         {   // Raihan Carder
-
-            Random generator = new Random();
-            List<int> numbers = new List<int>();
-            int randomValue, selection, newValue = 0, amountNumbers = 25;
-            string punctuation, changeDescription = "Here are your list of integers";
-            bool quit = false;
-
             Console.Title = "Lists";
 
-            // create static method for this code
+            ListOfIntegers();
+            Console.ReadLine();
+        }
+
+        public static void ListOfIntegers()
+        {
+            Random generator = new Random();
+            List<int> numbers = new List<int>();
+            int randomValue, selection, newValue = 0, amountNumbers = 25, removeValue, amountRemoved = 0, fifteenOccurrences = 0, numberOccurences, selectedOccurence;
+            string punctuation, changeDescription = "Here are your list of integers";
+            bool quit = false;
 
             for (int i = 0; i < 25; i++)
             {
@@ -29,6 +34,16 @@ namespace Lists_Assignment
 
             while (!quit)
             {
+                amountRemoved = 0;
+                fifteenOccurrences = 0;
+                numberOccurences = 0;
+
+                if (!numbers.Any())
+                {
+                    changeDescription = "Please press 2 to create a new list or Add some New Values";
+                }
+
+
                 Console.WriteLine(changeDescription);
                 Console.WriteLine();
                 Console.WriteLine("---------------------------------------------------------------------------------------------------");
@@ -63,12 +78,12 @@ namespace Lists_Assignment
                 Console.WriteLine("11. How many times is ___ in this list?");
                 Console.Write("Enter the integer corresponding with your decision: ");
 
-                if (int.TryParse(Console.ReadLine(), out selection))
+                if (int.TryParse(Console.ReadLine().Trim(), out selection))
                 {
                     if (selection == 1)
                     {
                         numbers.Sort();
-                        changeDescription = "Here's your new Sorted list: ";
+                        changeDescription = "Here's your Sorted list: ";
                     }
                     else if (selection == 2)
                     {
@@ -84,29 +99,65 @@ namespace Lists_Assignment
                     }
                     else if (selection == 3)
                     {
+                        Console.WriteLine();
+                        Console.Write("Enter a value you want removed: ");
+
+                        if (int.TryParse(Console.ReadLine().Trim(), out removeValue) && removeValue >= 10 && removeValue < 21)
+                        {
+                            for (int i = 0; i < amountNumbers; i++)
+                            {
+                                if (numbers.Contains(removeValue))
+                                {
+                                    numbers.Remove(removeValue);
+                                    amountRemoved += 1;
+                                }
+                            }
+                            amountNumbers -= amountRemoved;
+                            changeDescription = "Here's your new list: ";
+
+                        }
+                        else
+                        {
+                            changeDescription = "your value could not be removed, here's your list: ";
+                        }
+
 
                     }
                     else if (selection == 4)
                     {
                         Console.WriteLine();
-                        Console.Write("Add an integer Value to the list: ");
-                        if (int.TryParse(Console.ReadLine(), out newValue))
+                        Console.Write("Add an integer Value to the list within the range 1-20: ");
+                        if (int.TryParse(Console.ReadLine(), out newValue) && newValue >= 10 && newValue < 21)
                         {
                             numbers.Add(newValue);
+                            amountNumbers += 1;
+                            changeDescription = "Here's your new list: ";
                         }
-                        amountNumbers += 1;
+                        else
+                        {
+                            changeDescription = "Value could not be added, here's your list: ";
+                        }
+
                     }
                     else if (selection == 5)
                     {
+                        for (int i = 0; i < amountNumbers; i++)
+                        {
+                            if (numbers[i] == 15)
+                            {
+                                fifteenOccurrences += 1;
+                            }
+                        }
+                        changeDescription = $"The list contains fifteen; {fifteenOccurrences} times.";
 
                     }
                     else if (selection == 6)
                     {
-
+                        changeDescription = $"The list's highest value is {numbers.Max()}";
                     }
                     else if (selection == 7)
                     {
-
+                        changeDescription = $"The list's smallest value is {numbers.Min()}";
                     }
                     else if (selection == 8)
                     {
@@ -114,14 +165,45 @@ namespace Lists_Assignment
                     }
                     else if (selection == 9)
                     {
-
+                        changeDescription = $"The list's sum value is {numbers.Sum()}, the lists average is {Math.Round(numbers.Average(), 2)}.";
                     }
                     else if (selection == 10)
                     {
+                        Console.WriteLine();
 
+                        var groupedNumbers = numbers.GroupBy(n => n);
+                        var maxFrequency = groupedNumbers.Max(g => g.Count());
+                        var modes = groupedNumbers.Where(g => g.Count() == maxFrequency).Select(g => g.Key);
+                        Console.Write("The mode(s) are: ");
+                        foreach (var mode in modes)
+                        {
+                            Console.Write(mode + " ");
+                        }
+
+                        Console.WriteLine();
+                        Console.Write("Click Enter to Continue; ");
+                        Console.ReadLine();
                     }
                     else if (selection == 11)
                     {
+                        Console.WriteLine();
+                        Console.Write("Enter a value that you'd like to know how many times its in the loop: ");
+                        if (int.TryParse(Console.ReadLine().Trim(), out selectedOccurence))
+                        {
+                            for (int i = 0; i < amountNumbers; i++)
+                            {
+                                if (numbers[i] == selectedOccurence)
+                                {
+                                    numberOccurences += 1;
+                                }
+                            }
+                        }
+                        else
+                        {
+
+                        }
+                        changeDescription = $"The value: {selectedOccurence} is in the list {numberOccurences} times.";
+
 
                     }
                     else
@@ -130,15 +212,17 @@ namespace Lists_Assignment
                     }
                 }
                 else
-                {
-
-                }            
+                { 
+                   // make a changedescription tellng them to enter valids election
+                }             
                 Console.Clear();
-
             }
+            Console.Write("Click enter to return to menu:");
             Console.ReadLine();
+
+            
+
+
         }
-
-
     }
 }
